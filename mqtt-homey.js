83d9066a7b0550d8e0786db9f62fa669ce402e3b
meg768 <meg768@users.noreply.github.com>
2023-02-09 18:01:40 +0100

    -

--- mqtt-homey.js
+++ mqtt-homey.js
@@ -1,12 +1,64 @@
 #!/usr/bin/env node
 
 var Mqtt = require('mqtt');
-var MqttDispatch = require('./mqtt-dispatch.js');
+//var MqttDispatch = require('./mqtt-dispatch.js');
 
 const {io} = require("socket.io-client");
 
 require('dotenv').config();
 require('yow/prefixConsole')();
+
+
+
+var MqttDispatch = function(client) {
+
+	let topics = [];
+
+	client.addListener('message', (topic, message) => {
+	
+		function isMatch(A, B) {
+			var args = {};
+	
+			var A = A.split('/');
+			var B = B.split('/');
+	
+			if (A.length != B.length)
+				return null;
+	
+			for (let i = 0; i < A.length; i++) {
+				if (A[i] != B[i]) {
+					let match = B[i].match(/^:([a-zA-Z0-9]+)$/);
+	
+					if (!match)
+						return null;
+	
+					args[match[1]] = A[i];
+				}
+			}
+	
+			return args;
+		}
+
+		message = message.toString();
+
+		topics.forEach((item) => {
+			let match = isMatch(topic, item);
+
+			if (match) {
+				client.emit(item, message, match);
+			}
+		});
+
+	});
+
+	client.on = function(topic, fn) {
+		topics.push(topic);
+		client.addListener(topic, fn);
+	}
+
+	return client;
+}
+
 
 
 class App {
@@ -81,7 +133,7 @@ class App {
 
 	
 
-/*
+                // Publish all
 				for (let [deviceID, device] of Object.entries(devices)) {
 
 					if (device.capabilitiesObj) {
@@ -96,8 +148,8 @@ class App {
 	
 					}
 				}
-*/
-				
+
+                
 
 				for (let [deviceID, device] of Object.entries(devices)) {
 
@@ -110,8 +162,7 @@ class App {
 								this.publish(deviceCapabilityID, value);
 							});
 
-							//this.cache[topic] = true;
-							this.mqtt.subscribe(topic + 'X', {}, async () => {
+							this.mqtt.subscribe(topic, {}, async () => {
 	
 								await this.mqtt.on(topic, async (value) => {
 	
